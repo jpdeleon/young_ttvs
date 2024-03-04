cd2ed416495abcfbbb2fad6e7ed9f1e201e2ae16
jerome de leon <jpdeleon.bsap@gmail.com>
2024-03-04 18:31:58 +0900

    fix code

--- code/prepare_allesfit.sh
+++ code/prepare_allesfit.sh
@@ -1,6 +1,6 @@
 fp1=prepare_allesfit_toi.batch
 #cat ../data/tics_resonance_from_toi.txt | while read name; do echo ./prepare_allesfit.py -name \"$name\" -sector all -clobber -dir /home/jp/github/research/project/young_ttvs/allesfitter/known_ttvs; done > $fp1
-outdir=/home/jp/github/research/project/young_ttvs/allesfitter/toi_in_resonance
+outdir=~/github/research/project/young_ttvs/allesfitter/toi_in_resonance
 cat ../data/tois_resonance_from_toi.txt | while read toiid; do echo ./prepare_allesfit.py -toi $toiid -sector all -clobber -dir $outdir; done > $fp1
 fp2=run_allesfit_toi.batch
 #cat ../data/tics_resonance_from_toi.txt | while read name; do echo python /home/jp/github/research/project/young_ttvs/allesfitter/known_ttvs/$name/run.py; done > $fp2
